7c2dcbb145b9b7a93de64d563a43d457a055b6f8
unknown <max@.(none)>
2010-12-12 22:54:58 +0400

    We add counter

--- test1.py
+++ test1.py
@@ -1,5 +1,11 @@
 #Variation of programm with "While" cycle (to exit programm insert "PNH")
 n=raw_input("What is your name: ")
-while n != "PNH":
+i=1
+while i != 5 and n != "PNH":
 	print "Hello ", n
+	i += 1
 	n=raw_input("What is your name: ")
+
+
+print "You enter", i, "names"
+raw_input()
